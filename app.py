2a135c57489f06272e99fac2275e52b87014c3e0
Muhammad Saad <saad992.sm@gmail.com>
2023-08-08 23:26:54 +0500

    logic change and used create_csv_agent

--- app.py
+++ app.py
@@ -3,21 +3,22 @@ from langchain.document_loaders import CSVLoader
 from langchain.indexes import VectorstoreIndexCreator
 from langchain.chains import RetrievalQA
 from langchain.llms import OpenAI
+from langchain.agents import create_csv_agent
+from langchain.agents.agent_types import AgentType
 import os
-# Put your open ai key in evironment variable OPENAI_API_KEY
-from dotenv import load_dotenv
-load_dotenv()
 
 app = Flask(__name__, static_folder='static', template_folder='templates')
 
-loader = CSVLoader(file_path='data/test_data.csv')
-index_creator = VectorstoreIndexCreator()
-docsearch = index_creator.from_loaders([loader])
-chain = RetrievalQA.from_chain_type(llm=OpenAI(), chain_type="stuff", retriever=docsearch.vectorstore.as_retriever(), input_key="question")
+os.environ["OPENAI_API_KEY"] = "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"
+# loader = CSVLoader(file_path='data/test_data.csv')
+# index_creator = VectorstoreIndexCreator()
+# docsearch = index_creator.from_loaders([loader])
+# chain = RetrievalQA.from_chain_type(llm=OpenAI(), chain_type="stuff", retriever=docsearch.vectorstore.as_retriever(), input_key="question")
+agent = create_csv_agent(OpenAI(temperature=0), 'data/test_data.csv', verbose=True,agent_type=AgentType.ZERO_SHOT_REACT_DESCRIPTION)
 
 def generate_response(query):
-    response = chain({"question": query})
-    return response['result']
+    response = agent.run(query)
+    return response
 
 @app.route("/")
 def index():
@@ -34,3 +35,43 @@ def chat():
 
 if __name__ == "__main__":
     app.run(debug=True)
+
+
+"""
+Based on the provided code snippet and context, here is one way to modify the RetrievalQA chain to remember old context when answering new questions:
+
+from langchain import ConversationChain, OpenAI
+from langchain.memories import ConversationBufferMemory
+
+# Create memory
+memory = ConversationBufferMemory(memory_key="conversation") 
+
+# Create RetrievalQA chain
+chain = RetrievalQA.from_chain_type(
+    llm=OpenAI(),
+    chain_type="stuff",
+    retriever=docsearch.vectorstore.as_retriever(),
+    input_key="question"  
+)
+
+# Wrap in ConversationChain to track context
+conv_chain = ConversationChain(chain=chain, memory=memory)
+
+# Sample usage
+response1 = conv_chain.predict(question="What is the capital of France?") 
+
+response2 = conv_chain.predict(question="What is its population?")
+The key steps are:
+
+Create a ConversationBufferMemory to store conversation history
+
+Create the normal RetrievalQA chain
+
+Wrap the RetrievalQA chain in a ConversationChain and pass the memory
+
+Each call to predict will now save context to memory
+
+The chain can leverage the conversation context when answering new questions
+
+This allows the RetrievalQA chain to maintain state and remember previous questions/answers to have more natural conversations.
+"""
